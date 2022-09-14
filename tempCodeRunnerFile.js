516865fbcf46006ec8d2ef101d82dd7615af19e8
“jyoon42” <jyoon42@myseneca.ca>
2022-09-14 19:43:55 -0400

    modify

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,4 +1,4 @@
-/*********************************************************************************
+ res.send(
 *  WEB322 – Assignment 1 
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
 *  No part of this assignment has been copied manually or electronically from any other source
@@ -10,18 +10,4 @@ ____________ Date: _______2022,09,14_________
 *  Online (Cyclic) URL:  https://cautious-attire-jay.cyclic.app
 *
 ********************************************************************************/ 
-
-
-var HTTP_PORT = process.env.PORT || 8080;
-var express = require("express");
-var app = express();
-
-// setup a 'route' to listen on the default url path
-app.get("/", (req, res) => {
-
-
-res.send(""Jinhyeok Yoon - 145283206"");
-});
-
-// setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT);
+Jinhyeok Yoon - 145283206)
