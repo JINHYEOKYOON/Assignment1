33b71d2d04a5d70b898098d63b139cacd430e97f
“jyoon42” <jyoon42@myseneca.ca>
2022-09-14 19:32:40 -0400

    Modify

--- server.js
+++ server.js
@@ -4,7 +4,23 @@ var app = express();
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-  res.send("Jinhyeok Yoon - 145283206");
+  res.send(`
+  *  WEB322 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: __JINHYEOK YOON____________________ Student ID: __145283206
+____________ Date: _______2022,09,14_________
+*
+*  Online (Cyclic) URL:  https://cautious-attire-jay.cyclic.app
+*
+********************************************************************************/ 
+
+  
+  Jinhyeok Yoon - 145283206
+  
+  `);
 });
 
 // setup http server to listen on HTTP_PORT
